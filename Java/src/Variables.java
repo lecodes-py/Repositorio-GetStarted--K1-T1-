e40267622be9e68d69adfc1c73f4b52d47a67407
lecodespy <leticia.batista.bs@gmail.com>
2023-01-21 15:26:10 -0300

    Update Variables file

--- Java/src/Variables.java
+++ Java/src/Variables.java
@@ -1,11 +1,19 @@
 public class Variables {
     public static void main (String [] args){
-        int age;
-        String name;
-
-        age = 21;
-        name = "Letícia";
+        int age = 21;
+        String name = "Letícia";
 
         System.out.println("Olá! Me chamo " + name + " e tenho " + age + " anos de idade!");
+
+        // Exemplos de tipos de variáveis
+        byte byteVar = 127;
+        short shortVar = 1000;
+        int intVar = 10000;
+        long longVar = 1000000L;
+        float floatVar = 100.020F;
+        double doubleVar = 100.000;
+        boolean booVar = true;
+        char charVar = 'A';
+
     }
 }
