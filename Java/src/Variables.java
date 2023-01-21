d5e73571f4536f8260b7dbc25795deb7fd3788f5
lecodespy <leticia.batista.bs@gmail.com>
2023-01-21 14:48:40 -0300

    My first hello world

---
+++ Java/src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main (String [] args){
+        int age;
+        String name;
+
+        age = 21;
+        name = "Letícia";
+
+        System.out.println("Olá! Me chamo " + name + " e tenho " + age + " anos de idade!");
+    }
+}
