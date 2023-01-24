a8e29737240e34557a0615951f2d8fae70546593
lecodespy <leticia.batista.bs@gmail.com>
2023-01-23 21:14:47 -0300

    Upload edits on my Java Project

--- Java/src/Arrays.java
+++ Java/src/Arrays.java
@@ -1,6 +1,20 @@
 public class Arrays {
-
     public static void main (String[] args){
-
+        //Declarando uma variável do tipo array
+        String [] values = new String [25];
+        //Atribuindo valores para a variável
+        values [0] = "Letícia";
+        values [10] = "Emmanuel";
+        values [24] = "Marinete";
+        //Imprimindo dados (e testando a vazia)
+        System.out.println(values[0]);
+        System.out.println(values[10]);
+        System.out.println(values[1]);
+        //Desafio do curso
+        String [] words = new String[] {"My","Name", "is"};
+        System.out.println(words[2]);
+        //Declarando outro array com a variável words
+        words = new String [10];
+        System.out.println(words[3]);
     }
 }
